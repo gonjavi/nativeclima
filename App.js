b96e6151cf0d6b1c7899b22ee73381920ead6e80
manci <gmancillapaz@gmail.com>
2021-02-25 09:15:15 -0500

    aplicacion funcionando y cambia color de pantalla de acuerdo al clima obtenido de la api

--- App.js
+++ App.js
@@ -17,7 +17,8 @@ const App = () => {
     pais: ''
   });
   const [consultar, guardarConsultar] = useState(false);
-  const [resultado, guardarResultado] = useState({}); 
+  const [resultado, guardarResultado] = useState({});
+  const [bgcolor, guardarBgcolor] = useState('rgb(71, 149, 212)'); 
 
   const { ciudad, pais } = busqueda;
 
@@ -33,6 +34,19 @@ const App = () => {
         const resultado = await respuesta.json();
         guardarResultado(resultado);
         guardarConsultar(false);
+        // modifica colores de fondo basado en temperatura
+        const kelvin = 273.15;
+        const { main } = resultado;
+        const actual = main.temp -kelvin;
+
+        if (actual < 10) {
+          guardarBgcolor('rgb(105, 108, 149)');
+        } else if (actual >= 10 && actual < 25) {
+          guardarBgcolor('rgb(71, 149, 212)');
+        } else {
+          guardarBgcolor('rgb(178, 28, 61)');
+        }
+
       } catch (error) {
         mostrarAlerta();
       }
@@ -53,10 +67,13 @@ const App = () => {
     Keyboard.dismiss();
   }
 
+  const bgColorApp = {
+    backgroundColor: bgcolor
+  }
   return (
     <>
     <TouchableWithoutFeedback onPress={() => ocultarteclado()}>
-      <View style={styles.app}>
+      <View style={[styles.app, bgColorApp]}>
         <View style={styles.contenido}>
           <Clima
             resultado={resultado}
@@ -76,7 +93,6 @@ const App = () => {
 const styles = StyleSheet.create({
   app: {
     flex: 1,
-    backgroundColor: 'rgb(71, 149, 212)',
     justifyContent: 'center'
   },
   contenido: {
